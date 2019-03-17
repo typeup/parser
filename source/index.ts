43f436e8c3b4b5f04a54dec7b8b438e080cb8f86
Simon Mika <simon@mika.se>
2019-03-17 13:47:40 +0100

    Removed wrapping class.

--- source/index.ts
+++ source/index.ts
@@ -4,18 +4,15 @@ import { Source } from "./Source"
 import { CommentStripper } from "./CommentStripper"
 import * as block from "./block"
 
-export class Parser {
-	constructor() {}
-	parse(reader: IO.Reader | undefined, handler: Error.Handler): dom.Document | undefined {
-		let result: dom.Document | undefined
-		if (reader) {
-			const source = new Source(new CommentStripper(reader), handler)
-			result = new dom.Document(block.parseAll(source) || [], source.mark())
-		}
-		return result
+export function parse(reader: IO.Reader | undefined, handler: Error.Handler): dom.Document | undefined {
+	let result: dom.Document | undefined
+	if (reader) {
+		const source = new Source(new CommentStripper(reader), handler)
+		result = new dom.Document(block.parseAll(source) || [], source.mark())
 	}
-	open(path: string | undefined, handler: Error.Handler): dom.Document | undefined {
-		const locator = Uri.Locator.parse(path)
-		return locator ? this.parse(IO.Reader.open(locator), handler) : undefined
-	}
+	return result
+}
+export function open(path: string | undefined, handler: Error.Handler): dom.Document | undefined {
+	const locator = Uri.Locator.parse(path)
+	return locator ? parse(IO.Reader.open(locator), handler) : undefined
 }
