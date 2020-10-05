111d9eb3600122c713621abace968eba87bc8574
Simon Mika <simon@mika.se>
2020-10-05 12:32:34 +0200

    Added test for html content in block.code.

--- block/code.spec.ts
+++ block/code.spec.ts
@@ -2,9 +2,13 @@ import { Error, IO } from "@cogneco/mend"
 import { Source } from "../Source"
 import { parse } from "."
 
-describe("block.diagram", () => {
+describe("block.code", () => {
 	it("simple", () => {
 		const result = parse(new Source(IO.StringReader.create("%%\ncode block\n%%\nFigure Caption."), new Error.ConsoleHandler())) || []
+		expect(result.map(node => node.toObject())).toMatchSnapshot()
+	})
+	it("html", () => {
+		const result = parse(new Source(IO.StringReader.create("%% html\n<html>\n<head>\n<title>Title</title>\n</head>\n<body>\n</body>\n</html>\n%%\ncode Caption."), new Error.ConsoleHandler())) || []
 		expect(result.map(node => node.toObject())).toMatchSnapshot()
 	})
 })
