eb49b1ed010d685ab4a78c030ec70a8b7eebde45
Simon Mika <simon@mika.se>
2026-05-05 07:58:54 +0200

    Improved tests.

--- index.spec.ts
+++ index.spec.ts
@@ -1,6 +1,10 @@
 import { parser } from "./index"
 
-const simpleDocument = `
+describe("parser", () => {
+	it.each([
+		{
+			name: "simple document",
+			document: `
 class = paper
 title = Document Title
 date = 2015-11-10
@@ -11,41 +15,7 @@ Paragraph A _emphasized text_ a lot of [http://example.com other nonsense]. This
 several lines and so
 it should. And here is %a small bit of code% and another one with a bit of formula: $x^2 + y^2 = z^2$.
 `
-
-describe("parser.parse", () => {
-	it("basic", () => {
-		const result = parser.parse(simpleDocument)
-		if (!result) expect(result)
-		else
-			expect(result.toObject()).toEqual({
-				class: "document",
-				content: [
-					{ class: "block.assignment", name: "class", value: "paper" },
-					{ class: "block.assignment", name: "title", value: "Document Title" },
-					{ class: "block.assignment", name: "date", value: "2015-11-10" },
-					{ class: "block.assignment", name: "author", value: "John Doe" },
-					{ class: "block.heading", content: [{ class: "inline.text", value: "Header Level 1" }], level: 1 },
-					{
-						class: "block.paragraph",
-						content: [
-							{ class: "inline.text", value: "Paragraph A " },
-							{ class: "inline.emphasize", content: [{ class: "inline.text", value: "emphasized text" }] },
-							{ class: "inline.text", value: " a lot of " },
-							{
-								class: "inline.link",
-								content: [{ class: "inline.text", value: "other nonsense" }],
-								target: "http://example.com"
-							},
-							{ class: "inline.text", value: ". This paragraph spans\n" },
-							{ class: "inline.text", value: "several lines and so\n" },
-							{ class: "inline.text", value: "it should. And here is " },
-							{ class: "inline.code", value: "a small bit of code" },
-							{ class: "inline.text", value: " and another one with a bit of formula: " },
-							{ class: "inline.math", value: "x^2 + y^2 = z^2" },
-							{ class: "inline.text", value: ".\n" }
-						]
-					}
-				]
-			})
-	})
+		}
+	] satisfies { name: string; document: string }[])("parse($name)", ({ document }) =>
+		expect(parser.parse(document)).toMatchSnapshot())
 })
