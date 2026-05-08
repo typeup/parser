ef83fd2c65739f4c50c68ba5c7042c4a0fe9e500
Simon Mika <simon@mika.se>
2026-05-08 14:35:37 +0200

    Improved link flags parsing.

--- inline/link.spec.ts
+++ inline/link.spec.ts
@@ -5,8 +5,13 @@ describe("parser.inline.link", () => {
 	it.each([
 		{ name: "basic", input: "[./destination link]" },
 		{ name: "in text", input: "This is a text with an [./destination link] in it." },
-		{ name: "single flag", input: "[./destination|external link]" },
-		{ name: "multiple flags", input: "[./destination|external|new-tab link]" },
-		{ name: "flag without display text", input: "[./destination|external]" }
+		{ name: "single flag", input: "[./destination|blank link]" },
+		{ name: "multiple flags", input: "[./destination|blank|download link]" },
+		{ name: "flag without display text", input: "[./destination|blank]" },
+		{ name: "unknown flag", input: "[./destination|unknown link]" },
+		{ name: "unknown and known flag", input: "[./destination|unknown|blank link]" },
+		{ name: "empty flag", input: "[./destination| link]" },
+		{ name: "empty then valid flag", input: "[./destination||blank link]" },
+		{ name: "empty then valid flag without display text", input: "[./destination||blank]" }
 	])("$name", ({ input }) => expect(parser.inline.parse(input)?.map(node => node.toObject())).toMatchSnapshot())
 })
--- inline/link.ts
+++ inline/link.ts
@@ -5,19 +5,35 @@ import { inline } from "./inline"
 function parse(source: Source): dom.Inline[] | undefined {
 	let result: dom.Inline[] | undefined
 	if (source.readIf("[")) {
-		const [target = "", ...flags] = (source.till([" ", "]"]).readAll() || "").split("|")
+		const s = source.till([" ", "]"])
+		const target = s.till("|").readAll() ?? ""
+		if (!target) s.raise("Empty link target.", "recoverable", "link-target-empty", s.mark())
+			const flags = parseFlags(s)
 		result = [
 			new dom.Inline.Link(
 				target,
 				source.readIf(" ")
 					? inline.parse(source.till("]")) || []
 					: [new dom.Inline.Text(target, source.mark()) as dom.Inline],
-				flags,
+					flags,
 				source.mark()
 			)
 		]
-		if (!source.readIf("]")) source.raise('Expected "]" as end of link.')
-	}
+		if (!source.readIf("]")) source.raise('Expected "]" as end of link.', "critical", "link-end", source.mark())
+		}
+	return result
+}
+function parseFlags(source: Source): dom.Inline.Link.Flag[] {
+	const result: dom.Inline.Link.Flag[] = []
+	if (source.readIf("|"))
+		do {
+			const flagSource = source.till("|")
+			const flagValue = flagSource.readAll()
+			if (dom.Inline.Link.Flag.is(flagValue)) {
+				result.push(flagValue)
+				flagSource.mark() // Advance marker baseline for next flag's error region
+			} else flagSource.raise(flagValue ? `Unknown link flag: ${flagValue}` : "Empty link flag.", "recoverable", "link-flag", flagSource.mark())
+		} while (source.readIf("|"))
 	return result
 }
 inline.addParser(parse)
